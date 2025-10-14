f519f348f744e9d843d815f7efb00e1be494a9c6
Guillermo Saez <guillesaez15@icloud.com>
2025-10-14 14:54:59 -0400

    Copy paste :)

--- nvim/init.lua
+++ nvim/init.lua
@@ -40,9 +40,9 @@ vim.o.showmode = false
 --  Schedule the setting after `UiEnter` because it can increase startup-time.
 --  Remove this option if you want your OS clipboard to remain independent.
 --  See `:help 'clipboard'`
--- vim.schedule(function()
---   vim.o.clipboard = 'unnamedplus'
--- end)
+vim.schedule(function()
+	vim.o.clipboard = "unnamedplus"
+end)
 
 -- Enable break indent
 vim.o.breakindent = true
@@ -107,6 +107,9 @@ vim.keymap.set({ "n", "v" }, "<leadder>d", '"_d')
 vim.keymap.set({ "n", "v" }, "<leader>y", '"+y')
 vim.keymap.set("n", "<leader>Y", '"+Y')
 
+-- Paste from the clipboard
+vim.keymap.set("n", "<leader>p", '"+p"')
+
 -- LOL
 vim.keymap.set("i", "<C-c>", "<Esc>")
 
@@ -164,6 +167,16 @@ vim.api.nvim_create_autocmd("TextYankPost", {
 	group = vim.api.nvim_create_augroup("kickstart-highlight-yank", { clear = true }),
 	callback = function()
 		vim.hl.on_yank()
+	end,
+})
+
+vim.api.nvim_create_autocmd({ "BufWritePre" }, {
+	desc = "Remove trailing whitspaces from buffer on write",
+	pattern = { "*" },
+	callback = function()
+		local save_cursor = vim.fn.getpos(".")
+		vim.cmd([[%s/\s\+$//e]])
+		vim.fn.setpos(".", save_cursor)
 	end,
 })
 
@@ -378,7 +391,7 @@ require("lazy").setup({
 			-- vim.keymap.set('n', '<leader>sf', builtin.find_files, { desc = '[Search] [F]iles' })
 			--
 			-- Grep
-			vim.keymap.set("n", "<leader>ps", function()
+			vim.keymap.set("n", "<leader>srg", function()
 				builtin.grep_string({ search = vim.fn.input("Grep > ") })
 			end)
 			vim.keymap.set("n", "<leader>sG", function()
