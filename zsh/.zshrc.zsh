5344a78c082e9e693033f621fee3a7c6dccf125b
Guillermo Saez <guillesaez15@icloud.com>
2026-02-11 11:27:30 -0500

    add editor

--- zsh/.zshrc.zsh
+++ zsh/.zshrc.zsh
@@ -53,3 +53,6 @@ export OBJC_DISABLE_INITIALIZE_FORK_SAFETY=YES
 export NVM_DIR="$HOME/.nvm"
 [ -s "$NVM_DIR/nvm.sh" ] && \. "$NVM_DIR/nvm.sh"  # This loads nvm
 [ -s "$NVM_DIR/bash_completion" ] && \. "$NVM_DIR/bash_completion"  # This loads nvm bash_completion
+
+export VISUAL=vim
+export EDITOR="$VISUAL"
