e7efb0410263ca77c27c8faf35e9d065cf38cd8a
Guillermo Saez <guillesaez15@icloud.com>
2026-02-13 14:16:23 -0500

    add expert for elixir instead of elixir ls

--- nvim/lua/plugins/elixir.lua
+++ nvim/lua/plugins/elixir.lua
@@ -1,10 +1,23 @@
 return {
-  "elixir-tools/elixir-tools.nvim",
-  dependencies = {
-    "nvim-treesitter/nvim-treesitter",
-    "neovim/nvim-lspconfig",
-  },
-  config = function()
-    require("elixir").setup()
-  end
+	"elixir-tools/elixir-tools.nvim",
+	dependencies = {
+		"nvim-treesitter/nvim-treesitter",
+		"neovim/nvim-lspconfig",
+	},
+	config = function()
+		local elixir = require("elixir")
+
+		elixir.setup({
+			-- Disable old servers
+			elixirls = { enable = false },
+			nextls = { enabled = false },
+		})
+
+		vim.lsp.config("expert", {
+			root_markers = { "mix.exs", ".git" },
+			filetypes = { "elixir", "eelixir", "heex" },
+		})
+
+		vim.lsp.enable("expert")
+	end,
 }
